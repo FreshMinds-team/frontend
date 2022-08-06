5b9f8f50650dccc324bafd93b509c66e694e34a3
Shishir Regmi <rshishir21@gmail.com>
2022-08-06 16:37:36 +0545

    'fdf'

---
+++ src/components/HomeView/HomeBanner/HomeBanner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HomeBanner = () => {
+  return (
+    <section className="section section-search">
+        <div className="container-fluid">
+          <div className="banner-wrapper">
+            <div className="banner-header text-center">
+              <h1>Search Doctor, Make an Appointment</h1>
+              <br />
+              <p>
+                Whether you’re looking to better understand your feelings or
+                experiencing anxiety, depression or high levels of stress,
+                <strong>Fresh Minds</strong> is designed to help you feel
+                better.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default HomeBanner
---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Clinic from './Clinic/Clinic'
+import Features from './Features/Features'
+import HomeBanner from './HomeBanner/HomeBanner'
+import Popular from './Popular/Popular'
+
+const HomeView = () => {
+  return (
+    <div>
+      <HomeBanner />
+      <Clinic />
+      <Popular />
+      <Features />
+    </div>
+  )
+}
+
+export default HomeView
